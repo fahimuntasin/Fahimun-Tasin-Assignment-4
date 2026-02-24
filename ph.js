2941a45d500f09c8c5d224c128bac3320af586ca
fahimuntasin <tasinfahimun@gmail.com>
2026-02-24 21:56:07 +0600

    finally done

--- ph.js
+++ ph.js
@@ -107,4 +107,51 @@ function deleteJob(id) {
       card.className = "card bg-base-100 shadow";
 
 
-  
+     card.innerHTML = `
+  <div class="card-body flex justify-between">
+
+    <!-- LEFT SIDE (All Job Content) -->
+    <div class="flex-1">
+      <h2 class="card-title">${job.company}</h2>
+      <p class="text-sm opacity-70">${job.role}</p>
+      <p class="text-xs opacity-60">
+        ${job.location} • ${job.type} • ${job.salary}
+      </p>
+
+      <div class="mt-2">
+        <span class="badge ${badgeClass}">${badgeText}</span>
+      </div>
+
+      <p class="mt-3">${job.description}</p>
+
+      <div class="card-actions mt-4 flex-wrap gap-2">
+        <button class="btn btn-outline btn-success btn-sm"
+          onclick="updateStatus(${job.id}, 'interview')">
+          Interview
+        </button>
+
+        <button class="btn btn-outline btn-error btn-sm"
+          onclick="updateStatus(${job.id}, 'rejected')">
+          Rejected
+        </button>
+      </div>
+    </div>
+
+    <!-- RIGHT SIDE (VERTICALLY CENTERED DELETE) -->
+    <div class="flex items-center ml-240">
+      <button class="btn btn-error btn-sm"
+        onclick="deleteJob(${job.id})">
+        Delete
+      </button>
+    </div>
+
+  </div>
+`;
+      container.appendChild(card);
+    });
+  }
+
+  updateCounts();
+  renderJobs();
+
+
