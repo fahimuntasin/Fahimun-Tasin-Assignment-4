1d5ba8e1cb059ac30e21af2fda5ea0c83810e529
fahimuntasin <tasinfahimun@gmail.com>
2026-02-24 18:40:52 +0600

    js first

---
+++ ph.js
@@ -0,0 +1,15 @@
+const jobs = [
+    {
+      id: 1,
+      company: "Mobile First Corp",
+      role: "React Native Developer",
+      location: "Remote",
+      type: "Full-time",
+      salary: "$130,000 - $175,000",
+      status: "not_applied",
+      description: "Build cross-platform mobile apps using React Native."
+    },
+]
+
+
+console.log(jobs)
